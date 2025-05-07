1c2dfa1d9f04d6ddc40b773d0d3b89a635e861f2
naveensainijpr@gmail.com <naveensainijpr@gmail.com>
2025-05-07 20:33:18 +0530

    firs

--- src/routes/backend/ReactNotes.routes.js
+++ src/routes/backend/ReactNotes.routes.js
@@ -1,26 +1,27 @@
 const express = require('express');
 const route = express.Router();
 const ReactNotesController = require('../../controllers/backend/ReactNotes.controller')
-
+const multer  = require('multer')
+const upload = multer()
 
 
 module.exports = app => {
 
 
-    route.post('/add', ReactNotesController.create); //http://localhost:5000/api/backend/ReactNotes/add
+    route.post('/add',upload.none(), ReactNotesController.create); //http://localhost:5000/api/backend/ReactNotes/add
 
-    route.post('/view', ReactNotesController.view);  //http://localhost:5000/api/backend/ReactNotes/view
+    route.post('/view',upload.none(), ReactNotesController.view);  //http://localhost:5000/api/backend/ReactNotes/view
 
-    route.post('/details/:id', ReactNotesController.details) // http://localhost:5000/api/backend/ReactNotes/details
+    route.post('/details/:id',upload.none(), ReactNotesController.details) // http://localhost:5000/api/backend/ReactNotes/details
 
-    route.put('/update', ReactNotesController.update) // http://localhost:5000/api/backend/ReactNotes/update
+    route.put('/update',upload.none(), ReactNotesController.update) // http://localhost:5000/api/backend/ReactNotes/update
 
-    route.put('/change-status', ReactNotesController.changeStatus) // http://localhost:5000/api/backend/ReactNotes/change-status
+    route.put('/change-status',upload.none(), ReactNotesController.changeStatus) // http://localhost:5000/api/backend/ReactNotes/change-status
 
-    route.delete('/delete/:id', ReactNotesController.delete)  //http://localhost:5000/api/backend/ReactNotes/delete
+    route.delete('/delete/:id',upload.none(), ReactNotesController.delete)  //http://localhost:5000/api/backend/ReactNotes/delete
 
 
-    route.post('/multiple-delete', ReactNotesController.multipleDelete)
+    route.post('/multiple-delete',upload.none(), ReactNotesController.multipleDelete)
 
     app.use('/api/backend/ReactNotes', route);
 
