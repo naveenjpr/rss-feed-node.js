d8e47a50abfac46b4e0216053feadf8181aa394f
naveenjpr <naveensainijpr@gmail.com>
2026-04-18 12:44:16 +0530

    react image add

--- src/routes/backend/ReactNotes.routes.js
+++ src/routes/backend/ReactNotes.routes.js
@@ -1,28 +1,31 @@
-const express = require('express');
+const express = require("express");
 const route = express.Router();
-const ReactNotesController = require('../../controllers/backend/ReactNotes.controller')
-const multer  = require('multer')
-const upload = multer()
+const ReactNotesController = require("../../controllers/backend/ReactNotes.controller");
+const multer = require("multer");
+const upload = require("../../config/upload");
 
+module.exports = (app) => {
+  route.post("/add", upload.array("images", 10), ReactNotesController.create); //http://localhost:5000/api/backend/ReactNotes/add
 
-module.exports = app => {
+  route.post("/view", upload.none(), ReactNotesController.view); //http://localhost:5000/api/backend/ReactNotes/view
 
+  route.post("/details/:id", upload.none(), ReactNotesController.details); // http://localhost:5000/api/backend/ReactNotes/details
 
-    route.post('/add',upload.none(), ReactNotesController.create); //http://localhost:5000/api/backend/ReactNotes/add
+  route.put(
+    "/update/:id",
+    upload.array("images", 10),
+    ReactNotesController.update,
+  ); // http://localhost:5000/api/backend/ReactNotes/update
 
-    route.post('/view',upload.none(), ReactNotesController.view);  //http://localhost:5000/api/backend/ReactNotes/view
+  route.put("/change-status", upload.none(), ReactNotesController.changeStatus); // http://localhost:5000/api/backend/ReactNotes/change-status
 
-    route.post('/details/:id',upload.none(), ReactNotesController.details) // http://localhost:5000/api/backend/ReactNotes/details
+  route.delete("/delete/:id", upload.none(), ReactNotesController.delete); //http://localhost:5000/api/backend/ReactNotes/delete
 
-    route.put('/update/:id',upload.none(), ReactNotesController.update) // http://localhost:5000/api/backend/ReactNotes/update
+  route.delete(
+    "/delete-image",
+    upload.none(),
+    ReactNotesController.deleteSingleImage,
+  ); //http://localhost:5000/api/backend/ReactNotes/delete-image
 
-    route.put('/change-status',upload.none(), ReactNotesController.changeStatus) // http://localhost:5000/api/backend/ReactNotes/change-status
-
-    route.delete('/delete/:id',upload.none(), ReactNotesController.delete)  //http://localhost:5000/api/backend/ReactNotes/delete
-
-
-    route.post('/multiple-delete',upload.none(), ReactNotesController.multipleDelete)
-
-    app.use('/api/backend/ReactNotes', route);
-
-}
+  app.use("/api/backend/ReactNotes", route);
+};
