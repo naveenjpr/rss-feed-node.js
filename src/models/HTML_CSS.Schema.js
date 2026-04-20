ea9f25612a908e3ac0d5a1fe4dca0d7932d5fe19
naveenjpr <naveensainijpr@gmail.com>
2026-04-20 11:20:33 +0530

    html css image add

--- src/models/HTML_CSS.Schema.js
+++ src/models/HTML_CSS.Schema.js
@@ -1,35 +1,47 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const HTML_CSSSchema = new mongoose.Schema({
-    Question: {
-        type: String,
-        required: [true, ' react Question  is required'],
-    },
-    Answers: {
-        type: String,
-        required: [true, ' react Answers  is required'],
-    },
- 
-    status: {
-        type: Boolean,
-        default: true
-    },
-  
-    created_at: {
-        type: Date,
-        default: Date.now
-    },
-    updated_at: {
-        type: Date,
-        default: Date.now
-    },
-    deleted_at: {
-        type: Date,
-        default: ''
-    }
+  Question: {
+    type: String,
+    required: [true, " react Question  is required"],
+  },
+  Answers: {
+    type: String,
+    required: [true, " react Answers  is required"],
+  },
 
+  images: [
+    {
+      url: {
+        type: String,
+        default: "",
+      },
+      public_id: {
+        type: String,
+        default: "",
+      },
+    },
+  ],
+
+  status: {
+    type: Boolean,
+    default: true,
+  },
+
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+  updated_at: {
+    type: Date,
+    default: Date.now,
+  },
+  deleted_at: {
+    type: Date,
+    default: null,
+  },
 });
 
-const HTML_CSSModel = mongoose.model('HTML_CSSNotes', HTML_CSSSchema);
+const HTML_CSSModel = mongoose.model("HTML_CSSNotes", HTML_CSSSchema);
 
 module.exports = HTML_CSSModel;
--- src/routes/backend/HTML_CSS.routes .js
+++ src/routes/backend/HTML_CSS.routes .js
@@ -1,27 +1,32 @@
-const express = require('express');
+const express = require("express");
 const route = express.Router();
-const HTML_CSSController = require('../../controllers/backend/HTML_CSS.controller')
-const multer  = require('multer')
-const upload = multer()
+const HTML_CSSController = require("../../controllers/backend/HTML_CSS.controller");
+const multer = require("multer");
+const upload = require("../../config/upload");
 
+module.exports = (app) => {
+  route.post("/add", upload.array("images", 10), HTML_CSSController.create); //http://localhost:5000/api/backend/HTML_CSS/add
 
-module.exports = app => {
+  route.post("/view", upload.none(), HTML_CSSController.view); //http://localhost:5000/api/backend/HTML_CSS/view
 
+  route.post("/details/:id", upload.none(), HTML_CSSController.details); // http://localhost:5000/api/backend/HTML_CSS/details
 
-    route.post('/add',upload.none(), HTML_CSSController.create); //http://localhost:5000/api/backend/HTML_CSS/add
+  route.put(
+    "/update/:id",
+    upload.array("images", 10),
+    HTML_CSSController.update,
+  ); // http://localhost:5000/api/backend/HTML_CSS/update
 
-    route.post('/view',upload.none(), HTML_CSSController.view);  //http://localhost:5000/api/backend/HTML_CSS/view
+  route.put("/change-status", upload.none(), HTML_CSSController.changeStatus); // http://localhost:5000/api/backend/HTML_CSS/change-status
 
-    route.post('/details/:id',upload.none(), HTML_CSSController.details) // http://localhost:5000/api/backend/HTML_CSS/details
+  route.delete("/delete/:id", upload.none(), HTML_CSSController.delete); //http://localhost:5000/api/backend/HTML_CSS/delete
 
-    route.put('/update/:id',upload.none(), HTML_CSSController.update) // http://localhost:5000/api/backend/HTML_CSS/update
+  route.delete(
+    "/delete-image",
+    upload.none(),
+    HTML_CSSController.deleteSingleImage,
+  );
+  //http://localhost:5000/api/backend/HTML_CSS/delete-image
 
-    route.put('/change-status',upload.none(), HTML_CSSController.changeStatus) // http://localhost:5000/api/backend/HTML_CSS/change-status
-
-    route.delete('/delete/:id',upload.none(), HTML_CSSController.delete)  //http://localhost:5000/api/backend/HTML_CSS/delete
-
-
-
-    app.use('/api/backend/HTML_CSS', route);
-
-}
+  app.use("/api/backend/HTML_CSS", route);
+};
